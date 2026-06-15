2251ebd60d0643b2bfa754a1b58fb2b87b1e861a
giangian2 <gianluca.bianchi0899@gmail.com>
2026-06-15 11:55:49 +0200

    Updated Measurrement buffer to be blocking on MAX_SIZE reached

--- src/main/java/smartfab/model/edge/MeasurementBuffer.java
+++ src/main/java/smartfab/model/edge/MeasurementBuffer.java
@@ -9,6 +9,8 @@ import java.util.List;
 public class MeasurementBuffer implements Buffer{
 
     private final List<Measurement> measurements;
+    private final static int        MAX_SIZE = 8;
+    private final static int        CLEAR_SIZE = 4;
 
     public MeasurementBuffer(){
         this.measurements = new ArrayList<>();
@@ -16,24 +18,46 @@ public class MeasurementBuffer implements Buffer{
 
     @Override
     public synchronized void addMeasurement(Measurement m) {
+
+        //Wait to insert the new measurement until the consumer thread will process the window contained in the buffer
+        while(measurements.size() >= MAX_SIZE){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
         this.measurements.add(m);
         //release all blocked threads
         notifyAll();
+
         System.out.println("Added measurement "+m.value());
     }
 
     @Override
     public synchronized List<Measurement> readAllAndClear() {
-        while(measurements.size() == 0){
+        while(measurements.size() < MAX_SIZE){
             try {
-                //wait for new measurements inserted
+                //wait for new measurements inserted (the sliding window processor will receive a complete window)
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
         var copy = new ArrayList<>(this.measurements);
+
+        /**
+         * We need to "swap" the list in order to guarantee the OVERLAP of 50% of the sliding window
+         * Ex: m = [1,2,3,4,5,6,7,8]
+         * When some thread des the readAllAndClear(), it will return the whole buffer
+         * and the buffer will be: m = [5,6,7,8,]
+         */
         this.measurements.clear();
+        this.measurements.addAll(this.measurements.subList((CLEAR_SIZE-1), MAX_SIZE));
+
+        //Thread that clear the buffer will notify the sensor thread that will be in waiting for the insertion of the new measurememnt
+        notifyAll();
         return copy;
     }
 
