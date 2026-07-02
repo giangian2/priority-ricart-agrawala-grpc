3ed226ea105652da46569ebcda94174d05c13f80
giangian2 <gianluca.bianchi0899@gmail.com>
2026-07-02 17:24:45 +0200

    (fix+doc): enabled search, updated resources and README

--- src/main/java/smartfab/http/contorller/ProductionLineController.java
+++ src/main/java/smartfab/http/contorller/ProductionLineController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
